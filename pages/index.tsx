e3000cb1ec9945d52bd9d0749230ca50ecd3cc2e
joelkmc <jhon.cabagua@kmc.solutions>
2022-01-03 09:20:07 +0800

    Added tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,10 +23,7 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <h1 className="bg-red-500 p-10"> asdasda</h1>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
